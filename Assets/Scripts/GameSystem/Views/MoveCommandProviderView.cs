4ad6292b1939d2b2c62ff855b3b3bb17c4c48687
triktron <ele.trik@hotmail.com>
2021-01-09 16:59:05 +0100

    implemented video 24 - object pooling

--- Assets/Scripts/GameSystem/Views/MoveCommandProviderView.cs
+++ Assets/Scripts/GameSystem/Views/MoveCommandProviderView.cs
@@ -4,15 +4,15 @@ using System;
 using MoveSystem;
 using GameSystem.Modals;
 using System.Collections.Generic;
+using GameSystem.Utils;
 
 namespace GameSystem.Views
 {
+    [RequireComponent(typeof(ObjectPool))]
     public class MoveCommandProviderView : MonoBehaviour
     {
-        [SerializeField]
-        private MoveComandView _moveComandView = null;
-
         private List<MoveComandView> _moveComandsViews = new List<MoveComandView>();
+        private ObjectPool _pool;
 
         public IMoveCommandProvider<ChessPiece> Modal
         {
@@ -22,11 +22,16 @@ namespace GameSystem.Views
             }
         }
 
+        private void Start()
+        {
+            _pool = GetComponent<ObjectPool>();
+        }
+
         private void UpdateCommandProviderViews(IMoveCommandProvider<ChessPiece> moveCommandProvider)
         {
             foreach (var moveComandView in _moveComandsViews)
             {
-                GameObject.Destroy(moveComandView.gameObject);
+                moveComandView.gameObject.SetActive(false);
             }
 
             _moveComandsViews.Clear();
@@ -35,10 +40,10 @@ namespace GameSystem.Views
             {
                 foreach (var moveComand in moveCommandProvider.MoveCommands())
                 {
-                    var view = GameObject.Instantiate(_moveComandView, transform);
+                    var go = _pool.GetPooledObject();
+                    var view = go.GetComponent<MoveComandView>();
 
                     view.Modal = moveComand;
-
                     _moveComandsViews.Add(view);
                 }
             }
---
+++ Assets/Scripts/Utils/ObjectPool.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameSystem.Utils
+{
+    public class ObjectPool: MonoBehaviour
+    {
+        private List<GameObject> _pooledObjects = new List<GameObject>();
+
+        [SerializeField]
+        private int _amount = 10;
+
+        [SerializeField]
+        private GameObject _pooledObject = null;
+
+        [SerializeField]
+        private Transform _parent = null;
+
+        private void Start()
+        {
+            for (int count = 0; count < _amount; count++)
+            {
+                var go = GameObject.Instantiate(_pooledObject, _parent);
+                go.SetActive(false);
+                _pooledObjects.Add(go);
+            }
+        }
+
+        public GameObject GetPooledObject()
+        {
+            foreach (var pooledObject in _pooledObjects)
+            {
+                if (!pooledObject.activeInHierarchy)
+                {
+                    pooledObject.SetActive(true);
+                    return pooledObject;
+                }
+            }
+
+            return null;
+        }
+    }
+}
