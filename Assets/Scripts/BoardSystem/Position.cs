929877041bd7b4cb8564cc17ccaa13340bd29276
triktron <ele.trik@hotmail.com>
2021-01-09 20:28:41 +0100

    implemented video 27 - BreadthFirstAreaSearch

--- Assets/Scripts/BoardSystem/Position.cs
+++ Assets/Scripts/BoardSystem/Position.cs
@@ -7,5 +7,8 @@
 
         public static Position operator +(Position a, Position b)
         => new Position { X = a.X + b.X, Y = a.Y + b.Y };
+
+        public static Position operator -(Position a, Position b)
+        => new Position { X = a.X - b.X, Y = a.Y - b.Y };
     }
 }
---
+++ Assets/Scripts/GameSystem/Utils/BreadthFirstAreaSearch.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameSystem.Utils
+{
+    public class BreadthFirstAreaSearch<TPosition>
+    {
+        public delegate List<TPosition> NeighbourStrategy(TPosition from);
+
+        public delegate float DistanceStrategy(TPosition from, TPosition to);
+
+        private readonly NeighbourStrategy _neighbour;
+        private readonly DistanceStrategy _distance;
+
+        public BreadthFirstAreaSearch(NeighbourStrategy neighbour, DistanceStrategy distance)
+        {
+            _neighbour = neighbour;
+            _distance = distance;
+        }
+
+        public List<TPosition> Area(TPosition centerPosition, float maxDistance)
+        {
+            var nearbyPositions = new List<TPosition>();
+            nearbyPositions.Add(centerPosition);
+
+            var nodeToVisit = new Queue<TPosition>();
+            nodeToVisit.Enqueue(centerPosition);
+
+            while (nodeToVisit.Count > 0)
+            {
+                var currentNode = nodeToVisit.Dequeue();
+
+                var neigbours = _neighbour(currentNode);
+                foreach (var neighbour in neigbours)
+                {
+                    if (nearbyPositions.Contains(neighbour)) continue;
+                    if (_distance(centerPosition, neighbour) < maxDistance)
+                    {
+                        nearbyPositions.Add(neighbour);
+                        nodeToVisit.Enqueue(neighbour);
+                    }
+                }
+            }
+
+            return nearbyPositions;
+        }
+    }
+}
