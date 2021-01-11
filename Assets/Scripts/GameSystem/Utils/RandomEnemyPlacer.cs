ce51cba58d39293f563b49d56e06d914fe4cd5a9
triktron <ele.trik@hotmail.com>
2021-01-11 11:27:19 +0100

    added random enemy placer

---
+++ Assets/Scripts/GameSystem/Utils/RandomEnemyPlacer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using HexGrid;
+using BoardSystem;
+
+namespace GameSystem.Utils
+{
+    class RandomEnemyPlacer : MonoBehaviour
+    {
+        [SerializeField]
+        private int _boardSize = 3;
+
+        [SerializeField]
+        private int _enemyAmount = 8;
+
+        [SerializeField]
+        private GameObject _enemyPrefab = null;
+
+        private void Awake()
+        {
+            PlaceRandomEnemies();
+        }
+
+        private void PlaceRandomEnemies()
+        {
+            var spots = new List<CubicHexCoord>() { new CubicHexCoord() };
+            var board = BoardPositionHelper.GenerateBoard(_boardSize);
+
+            while (spots.Count <= _enemyAmount)
+            {
+                var go = Instantiate(_enemyPrefab);
+
+                CubicHexCoord spot = GetRandomSpot(board);
+                while (spots.Contains(spot)) spot = GetRandomSpot(board);
+
+                go.transform.position = BoardPositionHelper.BoardToWorldPosition(spot);
+                spots.Add(spot);
+            }
+        }
+
+        private static CubicHexCoord GetRandomSpot(List<CubicHexCoord> board)
+        {
+            return board[UnityEngine.Random.Range(0, board.Count)];
+        }
+    }
+}
